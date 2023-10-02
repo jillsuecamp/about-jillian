26814615c17b0b09f75c6c45737531f204d454fa
Jillian Camp <jillsue@udel.edu>
2023-10-02 01:24:38 -0400

    validatePhoneNumber(), logic for error displays

---
+++ src/validation.js
@@ -0,0 +1,18 @@
+const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+function validateEmail(email) {
+    return emailPattern.test(email);
+}
+
+window.validateEmail = validateEmail;
+
+
+function validatePhoneNumber(phoneNumber) {
+    const pattern = /^\d{3}-\d{3}-\d{4}$/;
+    return pattern.test(phoneNumber);
+  }
+ 
+window.validatePhoneNumber = validatePhoneNumber;
+
+
+  
